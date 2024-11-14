fb09e65c4626aecff98b7c25222b252d41d717f2
Bhavana <bhavananb2003@gmail.com>
2024-11-14 20:37:40 +0530

    Added

---
+++ Sorting/Bubblesort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+public class Bubblesort {
+    public static void bubble(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,6,1,8};
+        bubble(arr);
+        printarr(arr);
+    }
+}
